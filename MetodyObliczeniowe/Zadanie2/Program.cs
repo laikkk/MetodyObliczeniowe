9a4dd2dd5bc2bbb0b806931a9b0c8d24faaff616
pmroczek <pmroczek@outlook.com>
2014-12-03 21:47:40 +0100

    Add exercise 2
    Exercise 2:
    - Bisection
    - SecantMethods

---
+++ MetodyObliczeniowe/Zadanie2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadanie2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ReadData dataReader = new ReadData();
+
+			if (dataReader.ReadParameters())
+			{
+				Calculation calculation = new Calculation(dataReader.Epsilon);
+				var resultBisection = calculation.Bisection(dataReader.A, dataReader.B, dataReader.UserCounter);
+
+				Console.WriteLine();
+				if (resultBisection.Value.HasValue)
+					Console.WriteLine("Wynik bisekcji to: {0} w krokach {1}", resultBisection.Value.Value, resultBisection.Key);
+				else
+					Console.WriteLine("Nie znaleziono rozwiązania metodą bisekcji w {0} krokach", resultBisection.Key);
+
+				var resultSecantMethods = calculation.SecantMethods(dataReader.A, dataReader.B, dataReader.UserCounter);
+
+				Console.WriteLine();
+				if (resultSecantMethods.Value.HasValue)
+					Console.WriteLine("Wynik metody siecznych to: {0} w krokach {1}", resultSecantMethods.Value.Value, resultSecantMethods.Key);
+				else
+					Console.WriteLine("Nie znaleziono rozwiązania metodą siecznych w {0} krokach", resultSecantMethods.Key);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ MetodyObliczeniowe/Zadanie2/ReadData.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadanie2
+{
+	/// <summary>
+	/// Klasa pobierająca dane od użytkownika
+	/// </summary>
+	public class ReadData
+	{
+		#region Field
+
+		private double _a;
+		private double _b;
+		private double _epsilon;
+		private int _userCounter;
+
+		#endregion
+
+		#region Properites
+
+		/// <summary>
+		/// Wartość epsilon
+		/// </summary>
+		public double Epsilon
+		{
+			get { return _epsilon; }
+		}
+
+		/// <summary>
+		/// Wartość parametru a
+		/// </summary>
+		public double A
+		{
+			get { return _a; }
+		}
+
+		/// <summary>
+		/// Wartość parametru b
+		/// </summary>
+		public double B
+		{
+			get { return _b; }
+		}
+
+		/// <summary>
+		/// Ilość kroków do wykonania
+		/// </summary>
+		public int UserCounter
+		{
+			get { return _userCounter; }
+		}
+
+		#endregion
+
+		#region Method
+
+		/// <summary>
+		/// Funkcja wczytująca parametry
+		/// </summary>
+		/// <returns>Zwraca wartość true lub false w zależności czy dane zostały wczytane poprawnie</returns>
+		public bool ReadParameters()
+		{
+			bool isValid = true;
+
+			Console.Write("Podaj epsilon: ");
+			if (!Double.TryParse(Console.ReadLine(), out _epsilon))
+			{
+				Console.WriteLine("Niepoprawny format epsilon. Musi być więszy od 0 i mniejszy od 1");
+				isValid = false;
+			}
+
+			if (isValid)
+				if (_epsilon < 0 || _epsilon > 1)
+				{
+					Console.WriteLine("Epsilon musi być większy od 0 i mniejszy od 1");
+					isValid = false;
+				}
+
+			if (isValid)
+			{
+				Console.Write("Podaj a: ");
+				if (!Double.TryParse(Console.ReadLine(), out _a))
+				{
+					Console.WriteLine("Podano nieprawdidłową wartość parametru a");
+					isValid = false;
+				}
+			}
+
+			if (isValid)
+			{
+				Console.Write("Podaj b: ");
+				if (!Double.TryParse(Console.ReadLine(), out _b))
+				{
+					Console.WriteLine("Podano nieprawdidłową wartość parametru b");
+					isValid = false;
+				}
+			}
+
+			if (isValid)
+			{
+				Console.Write("Podaj ilość kroków które wykonać: ");
+				if (!Int32.TryParse(Console.ReadLine(), out _userCounter))
+				{
+					Console.WriteLine("Podano nieprawdidłową wartość parametru kroków");
+					isValid = false;
+				}
+				else if (_userCounter <= 0)
+				{
+					Console.WriteLine("Ilość kroków nie może być mniejsza od 0");
+					isValid = false;
+				}
+			}
+
+			return isValid;
+		}
+
+		#endregion
+
+		#region static method
+
+		public static int? ReadParameter()
+		{
+			int userCounter;
+			Console.Write("Podaj ilość kroków które wykonać: ");
+
+			if (!Int32.TryParse(Console.ReadLine(), out userCounter))
+			{
+				Console.WriteLine("Podano nieprawdidłową wartość parametru kroków");
+				return null;
+			}
+
+			if (userCounter <= 0)
+			{
+				Console.WriteLine("Ilość kroków nie może być mniejsza od 0");
+				return null;
+			}
+
+			return userCounter;
+		}
+
+		public static char ReadChar(int userCounter)
+		{
+			Console.Write("Wykonałem już {0}, kroków czy chesz kontynuować? T-tak, n-nie: ", userCounter);
+			var key = Console.ReadKey().KeyChar;
+			Console.WriteLine();
+			return key;
+		}
+
+		#endregion
+	}
+}
