da860c442fe87455be843fed1cbcdff2d4a6b780
laikkk <kamilamd@gmail.com>
2014-12-07 12:06:39 +0100

    Fixed SecantMethod and add some amendments

--- MetodyObliczeniowe/Zadanie2/Calculation.cs
+++ MetodyObliczeniowe/Zadanie2/Calculation.cs
@@ -1,131 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
 
 namespace Zadanie2
 {
-	/// <summary>
-	/// Klasa opisująca równanie 
-	/// </summary>
-	public class Calculation
-	{
-		private double _epsilon;
-		private const char Kontynuacja = 'T';
+    /// <summary>
+    /// Klasa opisująca równanie 
+    /// </summary>
+    public class Calculation
+    {
+        private double _epsilon;
+        private const char Kontynuacja = 'T';
 
-		public Calculation(double epsilon)
-		{
-			_epsilon = epsilon;
-		}
+        public Calculation(double epsilon)
+        {
+            _epsilon = epsilon;
+        }
 
-		private double CalculateFx(double a)
-		{
-			return Math.Pow(a, 5) + Math.Pow(a, 1) - 5;
-			//return Math.Pow(a, 3) - Math.Pow(a, 1) + 1;
-			//return Math.Pow(a, 3) - Math.Pow(a, 2) + 2 * a - 1;
-			//return a * a * a * (a + Math.Sin(a * a - 1) - 1) - 1; // wyniki są na przedziale [-1,0] i [1,2]
-		}
+        private double CalculateFx(double a)
+        {
+            return Math.Pow(a, 5) + Math.Pow(a, 1) - 5;
+            //return Math.Pow(a, 3) - Math.Pow(a, 1) + 1;
+            //return Math.Pow(a, 3) - Math.Pow(a, 2) + 2 * a - 1;
+            //return a * a * a * (a + Math.Sin(a * a - 1) - 1) - 1; // wyniki są na przedziale [-1,0] i [1,2]
+        }
 
-		/// <summary>
-		/// Metoda bisekcji
-		/// </summary>
-		/// <returns></returns>
-		internal KeyValuePair<int, double?> Bisection(double a, double b, int userCounter)
-		{
-			var fa = CalculateFx(a);
-			var fb = CalculateFx(b);
-			if (fa * fb > 0) return new KeyValuePair<int, double?>(userCounter, null);
-			int counter = 0;
-			while (counter < userCounter)
-			{
-				counter += 1;
-				var c = (a + b) / 2;
-				var fc = CalculateFx(c);
-				if (Math.Abs(fc) < _epsilon)
-					return new KeyValuePair<int, double?>(counter, c);
-				if (fa * fc < 0)
-				{
-					b = c;
-				}
-				else
-				{
-					a = c;
-					fa = fc;
-				}
+        /// <summary>
+        /// Metoda bisekcji
+        /// </summary>
+        /// <returns></returns>
+        internal KeyValuePair<int, double?> Bisection(double a, double b, int userCounter)
+        {
+            var fa = CalculateFx(a);
+            var fb = CalculateFx(b);
+            int counter = 0;
 
-				if (counter == userCounter)
-				{
-					if (!ExecuteNextsSteps(counter))
-						return new KeyValuePair<int, double?>(counter, null);
+            if (fa * fb > 0)
+            {
+                return new KeyValuePair<int, double?>(userCounter, null);
+            }
 
-					userCounter += GetNextStepsCount();
-				}
-			}
+            while (counter < userCounter)
+            {
+                counter += 1;
+                var c = (a + b) / 2;
+                var fc = CalculateFx(c);
+                if (Math.Abs(fc) < _epsilon)
+                {
+                    return new KeyValuePair<int, double?>(counter, c);
+                }
 
-			return new KeyValuePair<int, double?>(userCounter, null);
-		}
+                if (fa * fc < 0)
+                {
+                    b = c;
+                }
+                else
+                {
+                    a = c;
+                    fa = fc;
+                }
 
-		/// <summary>
-		/// Metoda sprawdzająca czy kontynuować obliczenia po wykonaniu n kroków
-		/// </summary>
-		/// <returns>zwraca true jeżeli użytkownik chce kontunuacji, false jeżeli użytkownik nie chce kontynuowac obliczeń</returns>
-		private bool ExecuteNextsSteps(int counter)
-		{
-			return ReadData.ReadChar(counter) == Kontynuacja;
-		}
+                if (counter == userCounter)
+                {
+                    if (!ExecuteNextsSteps(counter))
+                    {
+                        return new KeyValuePair<int, double?>(counter, null);
+                    }
 
-		/// <summary>
-		/// Pobiera ilość kolejnych kroków od użytkownika
-		/// </summary>
-		/// <returns></returns>
-		private int GetNextStepsCount()
-		{
-			var count = ReadData.ReadParameter();
-			return count.HasValue ? count.Value : 0;
-		}
+                    userCounter += GetNextStepsCount();
+                }
+            }
 
-		///// <summary>
-		///// Metoda dwusiecznych do znajdowania rozwiązania
-		///// </summary>
-		///// <returns>Zwraca null jeżeli nie znaleziono rozwiązania, gdy znajdzie rozwiązanie zwróci punkt</returns>
-		internal KeyValuePair<int, double?> SecantMethods(double a, double b, int userCounter)
-		{
-			var fa = CalculateFx(a);
-			var fb = CalculateFx(b);
-			int counter = 0;
+            return new KeyValuePair<int, double?>(userCounter, null);
+        }
 
-			while (counter < userCounter && Math.Abs(a - b) > _epsilon)
-			{
-				counter++;
+        /// <summary>
+        /// Metoda sprawdzająca czy kontynuować obliczenia po wykonaniu n kroków
+        /// </summary>
+        /// <returns>zwraca true jeżeli użytkownik chce kontunuacji, false jeżeli użytkownik nie chce kontynuowac obliczeń</returns>
+        private bool ExecuteNextsSteps(int counter)
+        {
+            return ReadData.ReadChar(counter) == Kontynuacja;
+        }
 
-				if (Math.Abs(fa - fb) < _epsilon)
-				{
-					return new KeyValuePair<int, double?>(userCounter, null);
-				}
+        /// <summary>
+        /// Pobiera ilość kolejnych kroków od użytkownika
+        /// </summary>
+        /// <returns></returns>
+        private int GetNextStepsCount()
+        {
+            var count = ReadData.ReadParameter();
+            return count.HasValue ? count.Value : 0;
+        }
 
-				var c = a - fa * ((a - b) / (fa - fb));
-				var fc = CalculateFx(c);
+        ///// <summary>
+        ///// Metoda siecznych
+        ///// </summary>
+        ///// <returns>Zwraca null jeżeli nie znaleziono rozwiązania, gdy znajdzie rozwiązanie zwróci punkt</returns>
+        internal KeyValuePair<int, double?> SecantMethods(double a, double b, int userCounter)
+        {
+            var fa = CalculateFx(a);
+            var fb = CalculateFx(b);
+            int counter = 0;
+            double c = 0;
 
-				if (Math.Abs(fc) < _epsilon)
-					return new KeyValuePair<int, double?>(counter, c);
+            while (counter < userCounter && Math.Abs(a - b) > _epsilon)
+            {
+                counter++;
 
-				b = a;
-				fb = fa;
-				a = c;
-				fa = fc;
+                if (Math.Abs(fa - fb) < _epsilon)
+                {
+                    Console.WriteLine("! Złe punkty startowe !");
+                    return new KeyValuePair<int, double?>(userCounter, null);
+                }
 
-				if (counter == userCounter)
-				{
-					if (!ExecuteNextsSteps(counter))
-						return new KeyValuePair<int, double?>(userCounter, null);
+                c = a - fa * ((a - b) / (fa - fb));
+                var fc = CalculateFx(c);
 
-					userCounter += GetNextStepsCount();
-				}
-			}
+                if (Math.Abs(fc) < _epsilon)
+                {
+                    return new KeyValuePair<int, double?>(counter, c);
+                }
 
-			return new KeyValuePair<int, double?>(userCounter, null);
-		}
-	}
+                b = a;
+                fb = fa;
+                a = c;
+                fa = fc;
+
+                if (counter == userCounter)
+                {
+                    if (!ExecuteNextsSteps(counter))
+                    {
+                        return new KeyValuePair<int, double?>(userCounter, null);
+                    }
+
+                    userCounter += GetNextStepsCount();
+                }
+            }
+
+            return new KeyValuePair<int, double?>(counter, c); //!
+        }
+    }
 }
