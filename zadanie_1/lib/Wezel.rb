810b2413b8935ba1be4192988fc0100d96e2d34e
pmroczek <pmroczek@outlook.com>
2015-01-04 15:30:48 +0100

    Add wezel class

---
+++ zadanie_1/lib/Wezel.rb
@@ -0,0 +1,66 @@
+class Wezel
+	attr_accessor :Potega
+	attr_accessor :Wspolczynnik
+	attr_accessor :WspolczynnikPomocniczy
+	attr_accessor :X
+	attr_accessor :WartoscWezla
+
+	def initialize(potega, x = "x", wspolczynnik = "", wspPom = 1.0) 
+    if(IsValidParameters(potega, x, wspolczynnik, wspPom))
+		  @Potega = potega
+		  @X = x
+		  @WspolczynnikPomocniczy = wspPom
+
+		  if(wspolczynnik == "")
+			  @Wspolczynnik = "a#{@Potega}"
+		  else
+			  @Wspolczynnik = wspolczynnik
+		  end
+    else
+      raise "Invalid argument"
+    end
+	end
+
+	def ToString 
+		if (@WspolczynnikPomocniczy == 0 || @Wspolczynnik == "0")
+			return
+		end
+		
+		if(@Wspolczynnik.include? "a")
+			rownianie = "#{@Wspolczynnik}"
+		else
+			wynik = @Wspolczynnik.to_f
+			rownianie = "#{wynik.round(2)}"
+		end
+
+		if(@WspolczynnikPomocniczy != 1)
+			rownianie += "*#{@WspolczynnikPomocniczy}"
+		end
+
+		rownianie += "*#{@X}^#{@Potega}"
+	end
+
+  private
+  
+    def IsValidParameters(potega, x, wspolczynnik, wspPom)
+      isValid = true
+    
+      if(!potega.is_a? Fixnum)
+        isValid = false
+      end
+
+      if(!x.is_a? String)
+        isValid = false
+      end
+    
+      if(!wspolczynnik.is_a? String)
+        isValid = false
+      end
+
+      if(!wspPom.is_a? Float)
+        isValid = false
+      end
+
+      return isValid
+    end
+end
