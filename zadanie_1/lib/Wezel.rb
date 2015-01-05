967bfd91413186f29acf261f077bd88eabbef674
pmroczek <pmroczek@outlook.com>
2015-01-05 16:48:07 +0100

    Fix calculation

--- zadanie_1/lib/Wezel.rb
+++ zadanie_1/lib/Wezel.rb
@@ -1,43 +1,44 @@
 class Wezel
-	attr_accessor :Potega
-	attr_accessor :Wspolczynnik
-	attr_accessor :WspolczynnikPomocniczy
-	attr_accessor :X
-	attr_accessor :WartoscWezla
+  attr_accessor :Potega
+  attr_accessor :Wspolczynnik
+  attr_accessor :WspolczynnikPomocniczy
+  attr_accessor :X
+  attr_accessor :WartoscWezla
 
-	def initialize(potega, x = "x", wspolczynnik = "", wspPom = 1.0) 
+  def initialize(potega, x = "x", wspolczynnik = "", wspPom = 1.0) 
     if(IsValidParameters(potega, x, wspolczynnik, wspPom))
-		  @Potega = potega
-		  @X = x
-		  @WspolczynnikPomocniczy = wspPom
+      @Potega = potega
+      @X = x
+      @WspolczynnikPomocniczy = wspPom
       @WartoscWezla = 0
-		  if(wspolczynnik == "")
-			  @Wspolczynnik = "a#{@Potega}"
-		  else
-			  @Wspolczynnik = wspolczynnik
-		  end
+      if(wspolczynnik == "")
+        @Wspolczynnik = "a#{@Potega}"
+      else
+        @Wspolczynnik = wspolczynnik
+      end
     else
       raise "Invalid argument"
     end
-	end
+  end
 
-	def ToString 
-		if (@WspolczynnikPomocniczy == 0.0 || @Wspolczynnik == "0.0")
-			return
-		end
-		if(@Wspolczynnik.to_s.include? "a")
-			rownianie = "#{@Wspolczynnik}"
-		else
-			wynik = @Wspolczynnik.to_f
-			rownianie = "#{wynik.round(2)}"
-		end
+  def ToString 
+    if (@WspolczynnikPomocniczy == 0.0 || @Wspolczynnik == "0.0")
+      return
+    end
 
-		if(@WspolczynnikPomocniczy != 1)
-			rownianie += "*#{@WspolczynnikPomocniczy}"
-		end
+    if(@Wspolczynnik.to_s.include? "a")
+      rownianie = "#{@Wspolczynnik}"
+    else
+      wynik = @Wspolczynnik.to_f
+      rownianie = "#{wynik.round(2)}"
+    end
 
-		rownianie += "*#{@X}^#{@Potega}"
-	end
+    if(@WspolczynnikPomocniczy != 1)
+      rownianie += "*#{@WspolczynnikPomocniczy}"
+    end
+
+    rownianie += "*#{@X}^#{@Potega}"
+  end
 
   private
   
